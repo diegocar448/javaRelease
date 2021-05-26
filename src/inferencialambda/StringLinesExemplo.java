6b95a222f01394fca306f1de2f1e5b71a76c8c57
Diego Cardoso <diegocar448@hotmail.com>
2021-05-26 17:06:09 -0300

    Teste 1

--- src/inferencialambda/StringLinesExemplo.java
+++ src/inferencialambda/StringLinesExemplo.java
@@ -1,10 +1,37 @@
 package inferencialambda;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Locale;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class StringLinesExemplo {
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         String html = "<html> <head> \n</head> \n<body> \n</body> \n</html>";
         System.out.println(html.lines().map(s -> "[TAG] :: "+s).collect(Collectors.joining()));
-    }
+    }*/
+
+
+
+    /*DESAFIO 1*/
+    /*public static void main(String[] args) throws IOException {
+
+        String entrada = "13";
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        entrada = br.readLine();
+        for (int i = 2; i <= Integer.parseInt(entrada); i++) {
+
+            if (i % 2 == 0) {
+
+                System.out.println(i);
+
+            }
+
+        }
+    }*/
+
+
 }
