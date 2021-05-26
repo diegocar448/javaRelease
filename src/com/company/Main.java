ffbe99571d17ed40181474560762e7d32a721e81
Diego Cardoso <diegocar448@hotmail.com>
2021-05-25 23:37:35 -0300

    Inferencia

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,19 +11,62 @@ import java.util.stream.Collectors;
 
 public class Main {
 
+    
+
     public static void main(String[] args) throws IOException {
-        URL url = new URL("https://docs.oracle.com/javase/10/language/");
-        URLConnection urlConnection = url.openConnection();
-
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-
-        System.out.println(bufferedReader.lines().collect(Collectors.joining()));
-
+        printarNomeCompleto("Joao", "silva");
+        printarSoma(2,2,4);
     }
 
 
-    protected void testar(){
+    public static void connectAndPringURLJavaOracle() throws IOException {
+        var url = new URL("https://docs.oracle.com/javase/10/language/");
+        var urlConnection = url.openConnection();
+
+        /*try {
+
+        }catch(Exception e){
+
+        }*/
+
+
+        var bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+        System.out.println(bufferedReader.lines().collect(Collectors.joining()).replaceAll(">", ">\n"));
     }
+
+
+    public static void printarNomeCompleto(String nome, String sobrenome){
+        var nomeCompleto = String.format("%s %s", nome, sobrenome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static void printarSoma(int... numeros) {
+        int soma;
+        if(numeros.length > 0){
+            soma = 0;
+            /*for (var numero : numeros){
+                soma+=numero;
+            }*/
+            for (var numero = 0; numero < numeros.length; numero++){
+                soma += numeros[numero];
+            }
+            System.out.println("valor::: " + soma);
+        }
+    }
+
+    //Consegue
+
+    //variaveis locais inicializadas
+    //variável suporte do enhaced for
+    //variável suporte do for iterativo
+
+
+
+    //Não Consegue
+
+    //var não pode ser utilizado em nivel de classe
+    //var não pode ser utilizado como parâmetro
+    //var nao pode ser utilizada em variaveis locais nao inicializadas
 
 
 }
